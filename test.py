0cfc9f9e188dcf5f4010c079a4a128f049b8ca1b
Alanmyl <34644622+Alanmyl@users.noreply.github.com>
2019-12-09 14:05:56 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -3,16 +3,14 @@ from Plots import *
 from Risk import *
 from utils import *
 from Estimate import *
-files = [pre_data(con.path+'data/'+path)
-         for path in os.listdir(con.path+'data/')]
-nums = [3 for x in files]
-paras = est_Ngbm_win(win_len=5, nums=nums, files=files)
-# estimate('RollWinNGBM',win_len=5,nums=nums,files=files)
-#risk = risk_Ngbm_mc_short('ES',0.975,paras,size=1000000)
-para = est_gbm_win(5, files[0])
-#test = risk_opt_stck('VaR',0.975,'put',0.01,para.iloc[:1])
-print(dt.datetime.now())
-#test = hedge_opt_stck('ES', 0.8, 0.975, 'put', para.iloc[:5], init=0.01)
-print(dt.datetime.now())
-bstest = backtest_stock('long', files[0])
-print(compare_backtest(bstest, risk_gbm('VaR', 0.95, para), 1))
+options = pre_data(con.path+'data/options/MMM_option.csv')
+stock = pre_data(con.path + 'data/stocks/MMM.csv')
+risk_free = pre_data(con.path + 'data/TNX.csv')['Close']
+start = dt.datetime.now()
+risk1 = risk_opt_stck('VaR', 0.95, 'put', 0.01, est_gbm_win(
+    5, stock), risk_free, options['volat'])
+print(dt.datetime.now() - start)
+risk2 = risk_opt_stck('VaR', 0.95, 'put', 0.0, est_gbm_win(
+    5, stock), risk_free, options['volat'])
+risk3 = risk_gbm('VaR', 0.95, est_gbm_win(
+    5, stock))
