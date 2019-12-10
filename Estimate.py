41a467023f13f063a24aa20204a25ecc7d03717c
Alanmyl <34644622+Alanmyl@users.noreply.github.com>
2019-12-09 23:46:17 -0500

    Add files via upload

--- Estimate.py
+++ Estimate.py
@@ -40,7 +40,8 @@ def est_gbm_exp(lam: Union[int, float], prices: pd.DataFrame, period: str = 'dai
         A DataFrame including necessary parameter columns with datetime as index and columns `logrtn`, `mu` and `sig`,
         where 'mu' and 'sig' are estimated values of GBM model.
     '''
-    assert period in ['daily', 'annual'], 'Wrong argument '+period
+    assert period in ['daily', 'annual'], 'Wrong argument ' + period
+    assert (lam >= 0) and (lam <= 1), 'Wrong argument '+lam
     df = logrtn(prices).to_frame(name='logrtn')
     df['sig'] = df['logrtn'].ewm(alpha=1-lam).std()
     df['mu'] = df['logrtn'].ewm(alpha=1-lam).mean() + \
@@ -52,25 +53,29 @@ def est_gbm_exp(lam: Union[int, float], prices: pd.DataFrame, period: str = 'dai
     return df.dropna()
 
 
-def est_2gbm_win(win_len: Union[int, float], num1, num2, file1: pd.DataFrame, file2: pd.DataFrame, period: str = 'daily'):
-    '''Estimate the parameters required in two stocks GBM model by averaging historical data in a rolling fixed-length window
+def est_2gbm_win(win_len: Union[int, float], num1, num2, file1: pd.Series, file2: pd.Series,
+                 period: str = 'daily', rho: bool = True) -> pd.DataFrame:
+    '''Estimate `mu`s and `sig`s required in two stocks GBM model by averaging historical data in a 
+    rolling fixed-length window.
 
     Args:
         win_len: number of years of the window length for smoothing.
-        num1: the number of stock1 at the beginning, it can be a number or an array-like type with the same length as `file1`.
-        num2: the number of stock2 at the beginning, it can be a number or an array-like type with the same length as `file2`.
-        file: the adjusted price of the financial assets.
+        num1, num2: see function `build_port2`.
+        file1, file2: the adjusted price of the financial assets.
         period: the default is the estimates of daily data, but user can also specify the period.
+        rho: whether to generate $\rho$ for the assumption that two stocks follow GBM and their
+         correlation of Brownian motion is $\rho$. Required by `risk_gbm_norm`.
 
     Returns:
-        A DataFrame including necessary parameter columns with datetime as index and columns `logrtn`, `mu` and `sig`,
-        where 'mu' and 'sig' are estimated values of GBM model.
+        A DataFrame with datetime as index and columns `logrtn`, `mu` and `sig`, where 'mu' and 'sig' are estimated values of GBM model.
     '''
+    assert period in ['daily', 'annual'], 'Wrong argument '+period
     df = pd.concat([est_gbm_win(win_len, file1), est_gbm_win(
         win_len, file2)], axis=1, join='inner')
     df.columns = ['logrtn1', 'sig1', 'mu1', 'logrtn2', 'sig2', 'mu2']
-    df['rho'] = ((df.logrtn1-df.logrtn1.mean())*(df.logrtn2-df.logrtn2.mean()
-                                                 )/df.sig1/df.sig2).rolling(window=5*con.year).mean()
+    if rho == True:
+        df['rho'] = ((df.logrtn1-df.logrtn1.mean())*(df.logrtn2-df.logrtn2.mean()
+                                                     )/df.sig1/df.sig2).rolling(window=5*con.year).mean()
     df['w'] = build_port2(num1, num2, file1, file2)['w'].loc[df.index]
     if period == 'annual':
         df.loc[:, 'sig'] = df.sig*con.year**0.5
@@ -78,9 +83,23 @@ def est_2gbm_win(win_len: Union[int, float], num1, num2, file1: pd.DataFrame, fi
     return df.dropna()
 
 
-def est_Ngbm_win(win_len, nums, files, period='daily'):
-    dfs = pd.concat([est_gbm_win(win_len, file)
-                     for file in files], axis=1, join='inner')
+def est_Ngbm_win(win_len: Union[int, float], nums, files, period='daily') -> pd.DataFrame:
+    '''function to estimate `mu`s and `sig`s in N stocks of GBM model by averaging historical data in a 
+    rolling fixed-length window.
+
+    Args:
+        win_len: number of years of the window length for smoothing.
+        nums: see function `build_portN`.
+        files: see function `build_portN`.
+        period: the default is the estimates of daily data, but user can also specify the period as annual.
+
+    Returns:
+        A DataFrame with datetime as index and columns `w1`, `logrtn1`, `mu1`, `sig1` and so on, where 'mu1' and 'sig1' are estimated values of GBM model.
+    '''
+    assert len(files) == len(
+        files), "Unconsistent lengths of arg files and nums"
+    dfs = pd.concat([est_gbm_win(win_len, x)
+                     for x in files], axis=1, join='inner')
     dfs.columns = [name+str(i+1) for i in range(len(files))
                    for name in ['logrtn', 'sig', 'mu']]
     dfs = pd.concat([dfs, build_portN(nums, files)], axis=1)
@@ -90,9 +109,20 @@ def est_Ngbm_win(win_len, nums, files, period='daily'):
     return dfs.dropna()
 
 
-def est_Ngbm_exp(lam, nums, files, period='daily'):
-    dfs = pd.concat([est_gbm_exp(lam, file)
-                     for file in files], axis=1, join='inner')
+def est_Ngbm_exp(lam: Union[int, float], nums, files, period='daily') -> pd.DataFrame:
+    '''function to estimate `mu`s and `sig`s in N stocks of GBM model by giving exponential weights to the historical data.
+
+    Args:
+        lam: the weight of past data, larger lambda leads to slower decay.
+        nums: see function `build_portN`.
+        files: see function `build_portN`.
+        period: the default is the estimates of daily data, but user can also specify the period as annual.
+
+    Returns:
+        A DataFrame with datetime as index and columns `w1`, `logrtn1`, `mu1`, `sig1` and so on, where 'mu1' and 'sig1' are estimated values of GBM model.
+    '''
+    dfs = pd.concat([est_gbm_exp(lam, x)
+                     for x in files], axis=1, join='inner')
     dfs.columns = [name+str(i+1) for i in range(len(files))
                    for name in ['logrtn', 'sig', 'mu']]
     dfs = pd.concat([dfs, build_portN(nums, files)], axis=1)
--- utils.py
+++ utils.py
@@ -40,19 +40,27 @@ def logrtn(data: Union[pd.DataFrame, pd.Series]) -> pd.Series:
         return pd.Series(np.log(data.iloc[1:].values / data.iloc[:-1].values), index=data.index[1:])
 
 
-def build_port2(num1, num2, file1:Union[pd.Series, pd.DataFrame], file2):
+def build_port2(num1, num2, file1: pd.Series, file2: pd.Series):
     '''function to calculate the values of a portfolio of two stocks.
-    
+
     Args:
-        num1: 
+        num1: the number of stock1 at the beginning, it can be a number or an array-like type with the same length as `file1`.
+        num2: the number of stock2 at the beginning, it can be a number or an array-like type with the same length as `file2`.
+        file1, file2: the adjusted price of the financial assets. It can be a sequence of price or a dataframe with a column named `adj_close`.
     '''
-    res = (file1.adj_close*num1+file2.adj_close *
-           num2).to_frame(name='adj_close')
-    res['w'] = file1.adj_close*num1/res.adj_close
+    res = (file1*num1+file2*num2).to_frame(name='adj_close')
+    res['w'] = file1*num1/res.adj_close
     return res.dropna()
 
 
-def build_portN(nums, files):
+def build_portN(nums, files) -> pd.DataFrame:
+    '''function to get the weights of each stock. A extension of `build_port2`.
+
+    Args:
+        nums: an array-like object containing numbers or series of numbers with the same length 
+        as that of the element in `files`, see function `build_port2`.
+        files: a sequence of Series as the price of assets.
+    '''
     summ = np.sum([files[i].adj_close*nums[i]
                    for i in range(len(files))], axis=0)
     res = pd.DataFrame()
@@ -61,7 +69,21 @@ def build_portN(nums, files):
     return res
 
 
-def bs_option(opt_type, r, sig, T, K, s0):
+def bs_option(opt_type: str, r, sig, T, K, s0) -> Union[pd.Series, float]:
+    '''function to calculate options price with Black-Scholes formula.
+
+    Args:
+        opt_type: `call` or `put`.
+        r: risk-free rate under the risk-neutral measure.
+        sig: volatility.
+        T: time to maturity.
+        K: strike price.
+        s0:initial price.
+
+    Returns:
+        A value or a sequence of values with datetime index.
+    '''
+    assert opt_type in ['call', 'put'], 'Wrong argument '+opt_type
     d1 = (np.log(s0/K)+(r+sig**2/2)*T)/sig/T**0.5
     d2 = d1-sig*T**0.5
     if opt_type == 'put':
